7bfc53925faf968535920b9913c376a710505970
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2020-08-18 20:49:52 -0400

    contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,78 +1,98 @@
 import React from 'react';
+
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
 import '../styles/ContactPage.css';
 
-import LocationIcon from '@material-ui/icons/LocationOn';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import PhoneIcon from '@material-ui/icons/Phone';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import FacebookIcon from '@material-ui/icons/Facebook';
 
 function ContactPage(props) {
     return(
-        <div className='section'>
-            <div className='contact'>
-                <div className='contact-info'>
-                   <div>
-                        <h2>Contact Info</h2>
-                        <ul className='info'>
-                            <li>
-                                <LocationIcon />
-                                1982 Lynch Street
-                                Fort Lauderdale Fl 53202
-                            </li>
-                            <li>
-                                <MailOutlineIcon />
-                                bionichouse@hotmail.com
-                            </li>
-                            <li>
-                                <PhoneIcon /> 
-                                920-913-3505
-                            </li>
-                        </ul>
-                   </div>
-                    <ul className='socials'>
-                        <li>
-                            <a href="https://www.linkedin.com" target="_blank">
-                                <LinkedInIcon aria-label="Linkedin" fontSize='large' />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https:/www.facebook.com" target="_blank">
-                                <FacebookIcon aria-label="Facebook" fontSize='large' />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className='contact-form'>
-                    <h2>Let's Talk</h2>
-                    <div className='form'>
-                        <div className='inputBox w50'>
-                            <input type='text' name='' required/>
-                                <span>First Name</span>
-                        </div>
-                        <div className='inputBox w50'>
-                            <input type='text' name='' required/>
-                                <span>Last Name</span>
-                        </div>
-                        <div className='inputBox w50'>
-                            <input type='email' name='' required/>
-                                <span>Email Address</span>
-                        </div>
-                        <div className='inputBox w50'>
-                            <input type='email' name='' required/>
-                                <span>Mobile Number</span>
-                        </div>
-                        <div className='inputBox w100'>
-                            <textarea name='' required></textarea>
-                                <span>Write your message here...</span>
-                        </div>
-                        <div className='inputBox w100'>
-                            <input type='submit' value='Send'/>
-                        </div>
+        <div>
+            <Container>
+                <Row>
+                    <Col lg={7} className="mx-auto">
+                        <Card className="shadow">
+                            <Card.Body>
+                                <Row className="pb-2">
+                                    <Col lg={12}>
+                                        <div className="text-center text-white py-3 head">
+                                            <h2>Contact Us</h2>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className="pt-4">
+                                    <Col lg={5} md={6}>
+                                        <Row className="px-3">
+                                            <Col lg={2}>
+                                                <PhoneInTalkIcon color="primary" style={{ fontSize: 27 }} />
+                                            </Col>
+                                            <Col lg={10}>
+                                                <h6 className="font-weight-bold pt-2">Give Us A Ring</h6>
+                                            </Col> 
+                                            <Col lg= {{ size: 4, offset: 1}} >
+                                                <p>
+                                                    Gary Architect <br />
+                                                    (954) 456-9827 <br />
+                                                    Mon-Fri, 8:00 - 5:00
+                                                </p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    
+                                    <Col md={6} lg={{ size: 6, offset: 1}}>
+                                        <Row className="px-3">
+                                            <Col lg={2}>
+                                                <LocationOnIcon color="primary" style={{ fontSize: 27 }} />
+                                            </Col>
+                                            <Col lg={10}>
+                                                <h6 className="font-weight-bold pt-2">Find Us In Office</h6>
+                                            </Col>
+                                            <Col lg={{ size: 5, offset: 1}} className="pl-4">
+                                                <p>
+                                                    2965 Illinois Avenue, <br />
+                                                    Boca Raton Fl 33013, <br />
+                                                    USA
+                                                </p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
 
-                    </div>
-                </div>
-            </div>
+                                <Form className="p-3">
+                                    <Row className="my-5">
+                                        <Col lg={6}>
+                                            <Form.Control bsPrefix="effect-1" type="text" placeholder="Full Name"/>
+                                            <span className="focus-border"></span>
+                                        </Col>
+                                        <Col lg={6}>
+                                            <Form.Control bsPrefix="effect-1" type="email" placeholder="Your Email"/>
+                                            <span className="focus-border"></span>
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="pt-3">
+                                        <Col lg={12}>
+                                            <Form.Control bsPrefix="effect-1" type="text" placeholder="Your Message"/>
+                                            <span className="focus-border"></span>
+                                        </Col>
+                                    </Row>
+
+                                    <Row>
+                                        <Col lg={6} className="mt-3">
+                                            <Button variant="primary" type="submit" bsPrefix="btn-1">
+                                                Send Message
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
